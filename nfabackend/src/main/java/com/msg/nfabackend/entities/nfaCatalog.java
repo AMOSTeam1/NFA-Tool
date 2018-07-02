e26e2bf9f0d0eef1dcf34dce75842e4ef65be497
allabors <allabors84@gmail.com>
2018-07-02 23:33:13 +0200

    intermediary commit before master merge

--- nfabackend/src/main/java/com/msg/nfabackend/entities/nfaCatalog.java
+++ nfabackend/src/main/java/com/msg/nfabackend/entities/nfaCatalog.java
@@ -1,11 +1,13 @@
 package com.msg.nfabackend.entities;
 
 import java.io.IOException;
-import java.io.Serializable;
 import java.io.StringWriter;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
-import java.util.Objects;
+import java.util.Map;
+import java.util.Map.Entry;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
@@ -17,249 +19,316 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.msg.nfabackend.templates.Template;
+import com.msg.nfabackend.templates.Template.Restriction;
 
 @Entity
-@Table(name ="nfa")
+@Table(name = "nfa")
 public class nfaCatalog {
-	
-	public static class BlueprintConverter implements AttributeConverter<NfaCatalogBlueprint, String> {
+
+	public static class BlueprintConverter implements AttributeConverter<Map<String, Template>, String> {
 		@Override
-		public String convertToDatabaseColumn(NfaCatalogBlueprint attribute) {
-			
+		public String convertToDatabaseColumn(Map<String, Template> attribute) {
+
 			if (attribute == null) {
 				return null;
 			}
-			
+
 			try (StringWriter stringWriter = new StringWriter()) {
 				new ObjectMapper().writeValue(stringWriter, attribute);
 				return stringWriter.toString();
 			} catch (IOException e) {
 				throw new IllegalStateException("Obj-to-JSON-Converting failed", e);
 			}
-			
+
 		}
 
+		@SuppressWarnings({ "unchecked", "rawtypes" })
 		@Override
-		public NfaCatalogBlueprint convertToEntityAttribute(String dbData) {
+		public Map<String, Template> convertToEntityAttribute(String dbData) {
+			if (dbData == null) {
+				return null;
+			}
 			try {
-				return dbData == null ? null 
-						: new ObjectMapper().readValue(dbData, NfaCatalogBlueprint.class);
+				ObjectMapper objectMapper = new ObjectMapper();
+				Map<String, Object> languages = objectMapper.readValue(dbData, HashMap.class);
+				for (Entry<String, ?> entry : languages.entrySet()) {
+					String templateString = convertToDatabaseColumn((Map) entry.getValue());
+					languages.replace(entry.getKey(), objectMapper.readValue(templateString, Template.class));
+				}
+				return (Map) languages;
 			} catch (IOException e) {
 				throw new IllegalStateException("JSON-to-Obj-Converting failed", e);
 			}
 		}
 	}
-	
+
 	public static class WertConverter implements AttributeConverter<List<String>, String> {
 		@Override
 		public String convertToDatabaseColumn(List<String> attribute) {
-			
+
 			if (attribute == null) {
 				return null;
 			}
-			
+
 			try (StringWriter stringWriter = new StringWriter()) {
 				new ObjectMapper().writeValue(stringWriter, attribute);
 				return stringWriter.toString();
 			} catch (IOException e) {
 				throw new IllegalStateException("Obj-to-JSON-Converting failed", e);
 			}
-			
+
 		}
 
 		@SuppressWarnings("unchecked")
 		@Override
 		public List<String> convertToEntityAttribute(String dbData) {
 			try {
-				return dbData == null ? null 
-						: (List<String>) new ObjectMapper().readValue(dbData, ArrayList.class)
-						.stream().map(e -> e == null ? null : e.toString()).collect(Collectors.toList());
+				return dbData == null ? null
+						: (List<String>) new ObjectMapper().readValue(dbData, ArrayList.class).stream()
+								.map(e -> e == null ? null : e.toString()).collect(Collectors.toList());
 			} catch (IOException e) {
 				throw new IllegalStateException("JSON-to-Obj-Converting failed", e);
 			}
 		}
 	}
-	
+
+	public static void main(String[] args) {
+		BlueprintConverter blueprintConverter = new BlueprintConverter();
+
+		Map<String, Template> hashMap = new HashMap<>();
+
+		Template value = new Template();
+		value.setName("Property Template");
+
+		Template.Property property = new Template.Property();
+
+		property.setNames(Arrays.asList("name 1"));
+		property.setValues(Arrays.asList("value 1"));
+
+		property.setRestrictions(Arrays.asList(Restriction.COMMA, Restriction.POINT));
+
+		value.setProperties(Arrays.asList(property));
+
+		hashMap.put("de", value);
+		hashMap.put("en", value);
+
+		String str = blueprintConverter.convertToDatabaseColumn(hashMap);
+
+		Map<String, Template> convertToEntityAttribute = blueprintConverter.convertToEntityAttribute(str);
+
+		convertToEntityAttribute.equals(hashMap);
+
+		blueprintConverter.convertToDatabaseColumn(convertToEntityAttribute);
+
+		convertToEntityAttribute.size();
+
+	}
+
 	public static class NfaCatalogBlueprint {
 		private BpPropertyTemplateNoCondition de;
 		private BpPropertyTemplateNoCondition en;
-		
+
 		/**
 		 * @return the de
 		 */
 		public BpPropertyTemplateNoCondition getDe() {
 			return de;
 		}
+
 		/**
-		 * @param de the de to set
+		 * @param de
+		 *            the de to set
 		 */
 		public void setDe(BpPropertyTemplateNoCondition de) {
 			this.de = de;
 		}
+
 		/**
 		 * @return the en
 		 */
 		public BpPropertyTemplateNoCondition getEn() {
 			return en;
 		}
+
 		/**
-		 * @param en the en to set
+		 * @param en
+		 *            the en to set
 		 */
 		public void setEn(BpPropertyTemplateNoCondition en) {
 			this.en = en;
 		}
-		
+
 		public void createDescription(List<String> wert) {
 			if (de.getErklaerung() == null) {
-				de.setErklaerung(String.join(" ", 
-						de.getCharacteristic(), de.getProperty(), de.getModalVerb(), de.getQualifiedValue(wert), de.getVerb()));
+				de.setErklaerung(String.join(" ", de.getCharacteristic(), de.getProperty(), de.getModalVerb(),
+						de.getQualifiedValue(wert), de.getVerb()));
 			}
 			if (en.getErklaerung() == null) {
-				en.setErklaerung(String.join(" ", 
-						en.getCharacteristic(), en.getProperty(), en.getModalVerb(), en.getVerb(), en.getQualifiedValue(wert)));
+				en.setErklaerung(String.join(" ", en.getCharacteristic(), en.getProperty(), en.getModalVerb(),
+						en.getVerb(), en.getQualifiedValue(wert)));
 			}
 		}
 	}
-	
+
 	public static class BpPropertyTemplateNoCondition {
 
 		private String bezeichnung;
-	    private String erklaerung;
-	    private String characteristic;
-	    private String property;
-	    private String modalVerb;
-	    private List<String> qualifyingEx;
-	    private String verb;
-	    
+		private String erklaerung;
+		private String characteristic;
+		private String property;
+		private String modalVerb;
+		private List<String> qualifyingEx;
+		private String verb;
+
 		/**
 		 * @return the bezeichnung
 		 */
 		public String getBezeichnung() {
 			return bezeichnung;
 		}
+
 		/**
-		 * @param bezeichnung the bezeichnung to set
+		 * @param bezeichnung
+		 *            the bezeichnung to set
 		 */
 		public void setBezeichnung(String bezeichnung) {
 			this.bezeichnung = bezeichnung;
 		}
+
 		/**
 		 * @return the erklaerung
 		 */
 		public String getErklaerung() {
 			return erklaerung;
 		}
+
 		/**
-		 * @param erklaerung the erklaerung to set
+		 * @param erklaerung
+		 *            the erklaerung to set
 		 */
 		public void setErklaerung(String erklaerung) {
 			this.erklaerung = erklaerung;
 		}
-		
+
 		/**
 		 * @return the characteristic
 		 */
 		public String getCharacteristic() {
 			return characteristic;
 		}
+
 		/**
-		 * @param characteristic the characteristic to set
+		 * @param characteristic
+		 *            the characteristic to set
 		 */
 		public void setCharacteristic(String characteristic) {
 			this.characteristic = characteristic;
 		}
+
 		/**
 		 * @return the property
 		 */
 		public String getProperty() {
 			return property;
 		}
+
 		/**
-		 * @param property the property to set
+		 * @param property
+		 *            the property to set
 		 */
 		public void setProperty(String property) {
 			this.property = property;
 		}
+
 		/**
 		 * @return the modalVerb
 		 */
 		public String getModalVerb() {
 			return modalVerb;
 		}
+
 		/**
-		 * @param modalVerb the modalVerb to set
+		 * @param modalVerb
+		 *            the modalVerb to set
 		 */
 		public void setModalVerb(String modalVerb) {
 			this.modalVerb = modalVerb;
 		}
+
 		/**
 		 * @return the qualifyingEx
 		 */
 		public List<String> getQualifyingEx() {
 			return qualifyingEx;
 		}
+
 		/**
-		 * @param qualifyingEx the qualifyingEx to set
+		 * @param qualifyingEx
+		 *            the qualifyingEx to set
 		 */
 		public void setQualifyingEx(List<String> qualifyingEx) {
 			this.qualifyingEx = qualifyingEx;
 		}
-		
+
 		/**
 		 * @return the verb
 		 */
 		public String getVerb() {
 			return verb;
 		}
+
 		/**
-		 * @param verb the verb to set
+		 * @param verb
+		 *            the verb to set
 		 */
 		public void setVerb(String verb) {
 			this.verb = verb;
 		}
-		
-		final String getQualifiedValue (List<String> wert) {
-			return IntStream.range(0, wert.size()).mapToObj(
-					i -> qualifyingEx.get(i) + " " + wert.get(i)).collect(Collectors.joining(" "));
+
+		final String getQualifiedValue(List<String> wert) {
+			return IntStream.range(0, wert.size()).mapToObj(i -> qualifyingEx.get(i) + " " + wert.get(i))
+					.collect(Collectors.joining(" "));
 		}
 	}
-	
+
 	@Id
-	@GeneratedValue(strategy=GenerationType.IDENTITY)
-	@Column (name ="nfa_id")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "nfa_id")
 	private Long nfaCatalogId;
-	
-	@Column (name ="NFA_NUMBER")
+
+	@Column(name = "NFA_NUMBER")
 	private Long nfaNumber;
-	
-	@Column (name ="nfa_type")
+
+	@Column(name = "nfa_type")
 	private String nfaCatalogType;
-	
-	@Column (name ="wert")
+
+	@Column(name = "wert")
 	@Convert(converter = WertConverter.class)
 	private List<String> nfaCatalogWert;
 
-	@Column (name ="rechtliche_verbindlichkeit")
+	@Column(name = "rechtliche_verbindlichkeit")
 	private String rechtlicheVerbindlichkeit;
-	
-	@Column (name ="formulierung")
+
+	@Column(name = "formulierung")
 	private String nfaCatalogFormulierung;
-	
-	@Column (name ="blueprint")
+
+	@Column(name = "blueprint")
 	@Convert(converter = BlueprintConverter.class)
-	private NfaCatalogBlueprint nfaCatalogBlueprint;
-	
-	@Column (name ="referenz")
+	private Map<String, Template> nfaCatalogBlueprint;
+
+	@Column(name = "referenz")
 	private String nfaCatalogReferenz;
-	
-	@Column (name ="referenzierte_projekte")
+
+	@Column(name = "referenzierte_projekte")
 	private String nfaCatalogReferenzierteProjekte;
-	
-	@Column (name ="kritikalitaet")
+
+	@Column(name = "kritikalitaet")
 	private String nfaCatalogKritikalität;
-	
-	@Column (name ="dokument")
+
+	@Column(name = "dokument")
 	private String nfaCatalogDokument;
 
 	public Long getNfaCatalogId() {
@@ -294,7 +363,6 @@ public class nfaCatalog {
 		this.nfaCatalogFormulierung = nfaCatalogFormulierung;
 	}
 
-
 	public String getNfaCatalogReferenz() {
 		return nfaCatalogReferenz;
 	}
@@ -326,7 +394,7 @@ public class nfaCatalog {
 	public void setNfaCatalogDokument(String nfaCatalogDokument) {
 		this.nfaCatalogDokument = nfaCatalogDokument;
 	}
-	
+
 	public List<String> getNfaCatalogWert() {
 		return nfaCatalogWert;
 	}
@@ -343,11 +411,11 @@ public class nfaCatalog {
 		this.nfaNumber = nfaNumber;
 	}
 
-	public NfaCatalogBlueprint getNfaCatalogBlueprint() {
+	public Map<String, Template> getNfaCatalogBlueprint() {
 		return nfaCatalogBlueprint;
 	}
 
-	public void setNfaCatalogBlueprint(NfaCatalogBlueprint nfaCatalogBlueprint) {
+	public void setNfaCatalogBlueprint(Map<String, Template> nfaCatalogBlueprint) {
 		this.nfaCatalogBlueprint = nfaCatalogBlueprint;
 	}
 }
