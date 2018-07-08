162ba744cca6d5854053456442dfc2b7a0dfec0b
allabors <allabors84@gmail.com>
2018-07-08 10:01:38 +0200

    backend import

--- nfabackend/src/main/java/com/msg/nfabackend/CORSFilter.java
+++ nfabackend/src/main/java/com/msg/nfabackend/CORSFilter.java
@@ -11,16 +11,16 @@ import javax.ws.rs.ext.Provider;
 
 @Provider
 public class CORSFilter implements ContainerResponseFilter {
-    
-    
-  @Override
-  public void filter(final ContainerRequestContext requestContext,
-                     final ContainerResponseContext cres) throws IOException {
-     cres.getHeaders().add("Access-Control-Allow-Origin", "*");
-     cres.getHeaders().add("Access-Control-Allow-Headers", "origin, content-type, accept, authorization");
-     cres.getHeaders().add("Access-Control-Allow-Credentials", "true");
-     cres.getHeaders().add("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD");
-     cres.getHeaders().add("Access-Control-Max-Age", "1209600");
-  }
+
+	@Override
+	public void filter(final ContainerRequestContext requestContext, final ContainerResponseContext cres)
+			throws IOException {
+		cres.getHeaders().add("Access-Control-Allow-Origin", "*");
+		cres.getHeaders().add("Access-Control-Allow-Headers",
+				"origin, content-type, accept, authorization, application/json, text/html");
+		cres.getHeaders().add("Access-Control-Allow-Credentials", "true");
+		cres.getHeaders().add("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD");
+		cres.getHeaders().add("Access-Control-Max-Age", "1209600");
+	}
 
 }
--- nfabackend/src/main/java/com/msg/nfabackend/resources/ProjectResource.java
+++ nfabackend/src/main/java/com/msg/nfabackend/resources/ProjectResource.java
@@ -1,5 +1,6 @@
 package com.msg.nfabackend.resources;
 
+import java.io.InputStream;
 import java.net.URI;
 import java.util.List;
 
@@ -19,6 +20,8 @@ import javax.ws.rs.core.UriInfo;
 
 import com.msg.nfabackend.entities.Project;
 import com.msg.nfabackend.services.QueryService;
+import com.sun.jersey.core.header.FormDataContentDisposition;
+import com.sun.jersey.multipart.FormDataParam;
 
 /**
  * JAX-RS-Resource for Entity 'project'
@@ -28,35 +31,33 @@ import com.msg.nfabackend.services.QueryService;
 @Consumes(MediaType.APPLICATION_JSON)
 @Produces(MediaType.APPLICATION_JSON)
 public class ProjectResource {
-	
+
 	@Inject
 	private QueryService queryService;
-	
+
 	@GET
 	public List<Project> getAllProject() {
 		return queryService.getAllProject();
 	}
-	
+
 	@POST
 	@Path("/create")
 	@Consumes(MediaType.APPLICATION_JSON)
 	@Produces(MediaType.APPLICATION_JSON)
 	public Response createProject(Project project, @Context UriInfo uriInfo) {
-		
+
 		Project createProject = queryService.createProject(project);
 		String newId = String.valueOf(createProject.getId());
-        URI uri = uriInfo.getAbsolutePathBuilder().path(newId).build();
-        return Response.created(uri)
-                .entity(createProject)
-                .build();
+		URI uri = uriInfo.getAbsolutePathBuilder().path(newId).build();
+		return Response.created(uri).entity(createProject).build();
 	}
-	
+
 	@DELETE
 	@Path("/{ProjectId}")
 	public void deleteProject(@PathParam("ProjectId") Long Id) {
-		 queryService.removeProject(Id);
+		queryService.removeProject(Id);
 	}
-	
+
 	@POST
 	@Path("/edit")
 	@Consumes(MediaType.APPLICATION_JSON)
@@ -64,11 +65,23 @@ public class ProjectResource {
 	public void editProject(Project project, @Context UriInfo uriInfo) {
 		queryService.updateProject(project);
 	}
-	
+
 	@GET
 	@Path("/search")
-	public List<Project> searchProject(@QueryParam(value = "status") String status,@QueryParam(value = "lookupCustName") String lookupCustName) {
-		return queryService.findProject(status,lookupCustName);
+	public List<Project> searchProject(@QueryParam(value = "status") String status,
+			@QueryParam(value = "lookupCustName") String lookupCustName) {
+		return queryService.findProject(status, lookupCustName);
 	}
-	
-} 
+
+	@POST
+	@Path("/importnfa")
+	@Produces({ MediaType.APPLICATION_JSON })
+	@Consumes({ MediaType.MULTIPART_FORM_DATA })
+	public Response upload(@FormDataParam("file") InputStream uploadedInputStream,
+			@FormDataParam("file") FormDataContentDisposition fileInputDetails) {
+
+		System.out.println("CONSUMED: " + fileInputDetails.getFileName());
+
+		return Response.status(200).entity("CONSUMED: " + fileInputDetails.getFileName()).build();
+	}
+}
